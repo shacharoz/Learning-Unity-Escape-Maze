ba3d98719142fc080fdbd99912384bec911cb040
Shachar Oz <shachar.oz@gmail.com>
2018-05-20 16:58:00 +0300

    init maze game

---
+++ Assets/Player/Movement.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float _speed;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //controlling the player up and down, left and right
+        Move();
+
+    }
+
+
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+        }
+    }
+ }
