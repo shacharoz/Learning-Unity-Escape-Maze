f876393b2c23090a67918de9deed62e3448f3587
Shachar Oz <shachar.oz@gmail.com>
2018-05-29 17:49:08 +0300

    adding trigger when exit the maze

---
+++ Assets/triggers/trigger door/TriggerManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerManager : MonoBehaviour {
+
+    public string ExitTargetTagName = "Exit";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == ExitTargetTagName)
+        {
+            Debug.Log("well done. you exited the maze");
+        }
+    }
+}
